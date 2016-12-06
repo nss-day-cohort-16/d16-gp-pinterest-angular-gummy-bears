5d8c53e20336ba3766b2baddcedf504e7fc0d5bf
Trent Hand <livefree1983@gmail.com>
2016-12-06 15:05:22 -0600

    Delete fb-getter.js

--- app/values/fb-getter.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-app.constant('FBCreds', {
-    apiKey: "AIzaSyDC_WZVweJMuNZfmXWbFHS6qkQak-goans",
-    authDomain: "pinterest-f8e85.firebaseapp.com",
-    databaseURL: "https://pinterest-f8e85.firebaseio.com"
-});
